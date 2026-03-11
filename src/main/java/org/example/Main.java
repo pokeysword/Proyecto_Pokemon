fdd75ece79c5e03f8ccb66f498582629f9dfe64d
Anxo Campos <anxocamposcastro2005@gmail.com>
2026-03-11 20:55:05 +0100

    arreglar los 179 errores

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,10 +4,10 @@ import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Scanner;
 
-import org.example.habilidades.Habilidad;
 
-import org.example.movimientos.CalmMind;
-import org.example.movimientos.Movimiento;
+import org.example.habilidades.*;
+
+import org.example.movimientos.*;
 
 
 public class Main {
@@ -15,35 +15,36 @@ public class Main {
     //Rotom
     ArrayList<Tipo> tiposRotom = new ArrayList<>();
     tiposRotom.add(Tipo.ELÉCTRICO);
-    tiposRotom.add(Tipo.AGUA); 
+    tiposRotom.add(Tipo.AGUA);
+
     ArrayList<Movimiento> movimientosRotom = new ArrayList<>();
-    movimientosRotom.add(HydroPump);
-    movimientosRotom.add(VoltSwitch);
-    movimientosRotom.add(WillOWisp);
-    movimientosRotom.add(Protect);
-    Pokemon RotomWash = new Pokemon("Rotom-Wash",50,tiposRotom,Levitate,157,85,128,125,128,106,movimientosRotom);
+    movimientosRotom.add(new HydroPump());
+    movimientosRotom.add(new VoltSwitch());
+    movimientosRotom.add(new WillOWisp());
+    movimientosRotom.add(new Protect());
+    Pokemon RotomWash = new Pokemon("Rotom-Wash",50,tiposRotom,new Levitate(),157,85,128,125,128,106,movimientosRotom);
 
     //Garchomp
     ArrayList<Tipo> tiposGarchomp = new ArrayList<>();
     tiposGarchomp.add(Tipo.DRAGÓN);
     tiposGarchomp.add(Tipo.TIERRA); 
     ArrayList<Movimiento> movimientosGarchomp = new ArrayList<>();
-    movimientosGarchomp.add(Earthquake);
-    movimientosGarchomp.add(DragonClaw);
-    movimientosGarchomp.add(RockSlide);
-    movimientosGarchomp.add(Protect);
-    Pokemon Garchomp = new Pokemon("Garchomp",50,tiposGarchomp,RoughSkin,183,182,115,95,105,154,movimientosGarchomp);
+    movimientosGarchomp.add(new Earthquake());
+    movimientosGarchomp.add(new DragonClaw());
+    movimientosGarchomp.add(new RockSlide());
+    movimientosGarchomp.add(new Protect());
+    Pokemon Garchomp = new Pokemon("Garchomp",50,tiposGarchomp,new RoughSkin(),183,182,115,95,105,154,movimientosGarchomp);
 
     //Togekiss
     ArrayList<Tipo> tiposTogekiss = new ArrayList<>();
     tiposTogekiss.add(Tipo.HADA);
     tiposTogekiss.add(Tipo.VOLADOR); 
     ArrayList<Movimiento> movimientosTogekiss = new ArrayList<>();
-    movimientosTogekiss.add(AirSlash);
-    movimientosTogekiss.add(DazzlingGleam);
-    movimientosTogekiss.add(CalmMind);
-    movimientosTogekiss.add(Protect);
-    Pokemon Togekiss = new Pokemon("Togekiss",50,tiposTogekiss,SereneGrace,191,90,115,140,135,113,movimientosTogekiss);
+    movimientosTogekiss.add(new AirSlash());
+    movimientosTogekiss.add(new DazzlingGleam());
+    movimientosTogekiss.add(new CalmMind());
+    movimientosTogekiss.add(new Protect());
+    Pokemon Togekiss = new Pokemon("Togekiss",50,tiposTogekiss,new SereneGrace(),191,90,115,140,135,113,movimientosTogekiss);
 
         
     //Metagross
@@ -51,31 +52,31 @@ public class Main {
     tiposMetagross.add(Tipo.ACERO);
     tiposMetagross.add(Tipo.PSÍQUICO); 
     ArrayList<Movimiento> movimientosMetagross = new ArrayList<>();
-    movimientosMetagross.add(MeteorMash);
-    movimientosMetagross.add(ZenHeadbutt);
-    movimientosMetagross.add(Earthquake);
-    movimientosMetagross.add(Protect);
-    Pokemon Metagross = new Pokemon("Metagross",50,tiposMetagross,ClearBody,187,178,150,105,110,110,movimientosMetagross);
+    movimientosMetagross.add(new MeteorMash());
+    movimientosMetagross.add(new ZenHeadbutt());
+    movimientosMetagross.add(new Earthquake());
+    movimientosMetagross.add(new Protect());
+    Pokemon Metagross = new Pokemon("Metagross",50,tiposMetagross,new ClearBody(),187,178,150,105,110,110,movimientosMetagross);
 
     //Milotic
     ArrayList<Tipo> tiposMilotic = new ArrayList<>();
     tiposMilotic.add(Tipo.AGUA); 
     ArrayList<Movimiento> movimientosMilotic = new ArrayList<>();
-    movimientosMilotic.add(Scald);
-    movimientosMilotic.add(IceBeam);
-    movimientosMilotic.add(Recover);
-    movimientosMilotic.add(Protect);
-    Pokemon Milotic = new Pokemon("Milotic",50,tiposMilotic,Competitive,202,90,125,135,145,101,movimientosMilotic);
+    movimientosMilotic.add(new Scald());
+    movimientosMilotic.add(new IceBeam());
+    movimientosMilotic.add(new Recover());
+    movimientosMilotic.add(new Protect());
+    Pokemon Milotic = new Pokemon("Milotic",50,tiposMilotic,new Competitive(),202,90,125,135,145,101,movimientosMilotic);
 
     //Arcanine
     ArrayList<Tipo> tiposArcanine = new ArrayList<>();
     tiposArcanine.add(Tipo.FUEGO);
     ArrayList<Movimiento> movimientosArcanine = new ArrayList<>();
-    movimientosArcanine.add(FlareBlitz);
-    movimientosArcanine.add(ExtremeSpeed);
-    movimientosArcanine.add(Snarl);
-    movimientosArcanine.add(WillOWisp);
-    Pokemon Arcanine = new Pokemon("Arcanine",50,tiposArcanine,Intimidate,187,155,120,115,120,135,movimientosArcanine);
+    movimientosArcanine.add(new FlareBlitz());
+    movimientosArcanine.add(new ExtremeSpeed());
+    movimientosArcanine.add(new Snarl());
+    movimientosArcanine.add(new WillOWisp());
+    Pokemon Arcanine = new Pokemon("Arcanine",50,tiposArcanine, new Intimidacion(),187,155,120,115,120,135,movimientosArcanine);
     
         
     //Amoonguss
@@ -83,11 +84,11 @@ public class Main {
     tiposAmoonguss.add(Tipo.PLANTA);
     tiposAmoonguss.add(Tipo.VENENO); 
     ArrayList<Movimiento> movimientosAmoonguss = new ArrayList<>();
-    movimientosAmoonguss.add(Spore);
-    movimientosAmoonguss.add(EnergyBall);
-    movimientosAmoonguss.add(RagePowder);
-    movimientosAmoonguss.add(Protect);
-    Pokemon Amoonguss = new Pokemon("Amoonguss",50,tiposAmoonguss,Regenerator,221,95,125,105,130,45,movimientosAmoonguss);
+    movimientosAmoonguss.add(new Spore());
+    movimientosAmoonguss.add(new EnergyBall());
+    movimientosAmoonguss.add(new RagePowder());
+    movimientosAmoonguss.add(new Protect());
+    Pokemon Amoonguss = new Pokemon("Amoonguss",50,tiposAmoonguss,new Regenerator(),221,95,125,105,130,45,movimientosAmoonguss);
 
 
     //Dragapult
@@ -95,11 +96,11 @@ public class Main {
     tiposDragapult.add(Tipo.DRAGÓN);
     tiposDragapult.add(Tipo.FANTASMA); 
     ArrayList<Movimiento> movimientosDragapult = new ArrayList<>();
-    movimientosDragapult.add(DragonPulse);
-    movimientosDragapult.add(ShadowBall);
-    movimientosDragapult.add(UTurn);
-    movimientosDragapult.add(Protect);
-    Pokemon Dragapult = new Pokemon("Dragapult",50,tiposDragapult,ClearBody,185,185,115,85,105,154,movimientosDragapult);
+    movimientosDragapult.add(new DragonPulse());
+    movimientosDragapult.add(new ShadowBall());
+    movimientosDragapult.add(new UTurn());
+    movimientosDragapult.add(new Protect());
+    Pokemon Dragapult = new Pokemon("Dragapult",50,tiposDragapult,new ClearBody(),185,185,115,85,105,154,movimientosDragapult);
 
         
     //Excadrill
@@ -107,22 +108,22 @@ public class Main {
     tiposExcadrill.add(Tipo.TIERRA);
     tiposExcadrill.add(Tipo.ACERO); 
     ArrayList<Movimiento> movimientosExcadrill = new ArrayList<>();
-    movimientosExcadrill.add(IronHead);
-    movimientosExcadrill.add(Earthquake);
-    movimientosExcadrill.add(SwordDance);
-    movimientosExcadrill.add(Protect);
-    Pokemon Excadrill = new Pokemon("Excadrill",50,tiposExcadrill,MoldBreaker,185,185,115,85,105,154,movimientosExcadrill);
+    movimientosExcadrill.add(new IronHead());
+    movimientosExcadrill.add(new Earthquake());
+    movimientosExcadrill.add(new SwordDance());
+    movimientosExcadrill.add(new Protect());
+    Pokemon Excadrill = new Pokemon("Excadrill",50,tiposExcadrill,new MoldBreaker(),185,185,115,85,105,154,movimientosExcadrill);
        
          
     //Sylveon
     ArrayList<Tipo> tiposSylveon = new ArrayList<>();
     tiposSylveon.add(Tipo.HADA);
     ArrayList<Movimiento> movimientosSylveon = new ArrayList<>();
-    movimientosSylveon.add(HyperVoice);
-    movimientosSylveon.add(MysticalFire);
-    movimientosSylveon.add(CalmMind);
-    movimientosSylveon.add(Protect);
-    Pokemon Sylveon = new Pokemon("Sylveon",50,tiposSylveon,Pixilate,185,185,115,85,105,154,movimientosSylveon);
+    movimientosSylveon.add(new HyperVoice());
+    movimientosSylveon.add(new MysticalFire());
+    movimientosSylveon.add(new CalmMind());
+    movimientosSylveon.add(new Protect());
+    Pokemon Sylveon = new Pokemon("Sylveon",50,tiposSylveon,new Pixilate(),185,185,115,85,105,154,movimientosSylveon);
        
     ArrayList<Pokemon> Equipo = new ArrayList<>();
     Boolean salir = false;
