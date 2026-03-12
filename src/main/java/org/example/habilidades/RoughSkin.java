3720bc028409e7e9a79abc3f780b213187662df5
Anxo Campos Castro <anxocamposcastro2005@gmail.com>
2026-03-12 11:23:24 +0100

    Arreglar el mensaje de RoughSkin

--- src/main/java/org/example/habilidades/RoughSkin.java
+++ src/main/java/org/example/habilidades/RoughSkin.java
@@ -12,7 +12,7 @@ public class RoughSkin extends Habilidad {
     public void efectoAlRecibirDaño(Pokemon rival, int daño, Movimiento move) {
         if (move.getCategoria().equals(Categoria.FISICO)){
         rival.setModPs((daño)/(rival.getModPs())-(daño/16));
-        System.out.println(rival.getNombre() + " se hizo daño al golpear"              + "! ¡Su Ataque bajó!");
+        System.out.println(rival.getNombre() + " se hizo daño al golpear");
     }}
 
     @Override
